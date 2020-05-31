93cb086db41020bafc49f3194a21ed829e152f57
skaeh <skaeh@DESKTOP-3DBKIHE>
2020-05-31 18:35:44 +0900

    새롭게 다시 올리고 머지를 한다.

--- git_project/src/chungLogin/LoginLogiz.java
+++ git_project/src/chungLogin/LoginLogiz.java
@@ -1,3 +1,4 @@
+
 package chungLogin;
 
 public class LoginLogiz {
---
+++ git_project/src/yijun2/yijun2.java
@@ -0,0 +1,7 @@
+package yijun2;
+
+public class yijun2 {
+	public static void main(String[] args) {
+		System.out.println("새로운파일");
+	}
+}
