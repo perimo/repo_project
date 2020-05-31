8d8a788305a2f3ce40aa910059ddc522d9d0b765
kcs <kcs@DESKTOP-5QS6AHC>
2020-05-31 18:16:35 +0900

    [20년05월31일] 충식 -로그인 작업

---
+++ git_project/src/chungLogin/LoginLogiz.java
@@ -0,0 +1,15 @@
+package chungLogin;
+
+public class LoginLogiz {
+public static String mem_id="test";
+public static String mem_pw="123";
+
+	public static void main(String[] args) {
+		if(mem_id.equals("test") && mem_pw.equals("123")) {
+			System.out.println("로그인");
+		}else {
+			System.out.println("로그인 실패");
+		}
+	}
+
+}
